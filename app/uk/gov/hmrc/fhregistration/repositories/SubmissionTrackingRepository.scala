d9551ce1ec65b37fbd8bd2f163523878038cc278
Cosmin Marian <cosmin.marian@digital.hmrc.gov.uk>
2018-04-09 14:44:55 +0100

    FHDDS-977: Add mongo store for submission tracking

---
+++ app/uk/gov/hmrc/fhregistration/repositories/SubmissionTrackingRepository.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistration.repositories
+
+import javax.inject.Inject
+
+import play.modules.reactivemongo.ReactiveMongoComponent
+import reactivemongo.api.indexes.{Index, IndexType}
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
+import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
+import uk.gov.hmrc.mongo.{AtomicUpdate, ReactiveRepository}
+
+import reactivemongo.play.json.ImplicitBSONHandlers._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
+
+class SubmissionTrackingRepository @Inject() (implicit rmc: ReactiveMongoComponent)
+  extends ReactiveRepository[SubmissionTracking, BSONObjectID](
+    "submission-tracking",
+    rmc.mongoConnector.db,
+    SubmissionTracking.formats,
+    ReactiveMongoFormats.objectIdFormats)
+
+    with AtomicUpdate[SubmissionTracking]
+{
+
+  override def isInsertion(newRecordId: BSONObjectID, oldRecord: SubmissionTracking): Boolean = oldRecord.id match {
+    case null ⇒ false
+    case id   ⇒ newRecordId.equals(id)
+  }
+
+  import SubmissionTracking.{FormBundleIdField, UserIdField}
+
+  def findSubmissionTrackingByUserId(userId: String) = {
+    collection.find(BSONDocument(UserIdField → userId)).one[SubmissionTracking]
+  }
+
+  def findSubmissionTrakingByFormBundleId(formBundleId: String) = {
+    collection.find(BSONDocument(FormBundleIdField → formBundleId)).one[SubmissionTracking]
+  }
+
+  def deleteSubmissionTackingByFormBundleId(formBundleId: String) = {
+    collection remove BSONDocument(FormBundleIdField → formBundleId)
+  }
+
+  def insertSubmissionTracking(submissionTracking: SubmissionTracking) = {
+    collection.insert(submissionTracking)
+  }
+
+
+
+  override def ensureIndexes(implicit ec: ExecutionContext): Future[Seq[Boolean]] = {
+    val indexes = Future sequence Seq(
+      collection.indexesManager.ensure(
+        Index(Seq(UserIdField -> IndexType.Ascending), name = Some("userIdIdx"), unique = false, sparse = true)),
+      collection.indexesManager.ensure(
+        Index(Seq(FormBundleIdField -> IndexType.Ascending), name = Some("formBundleIdIdx"), unique = true, sparse = true))
+    )
+
+    for {
+      s ← super.ensureIndexes
+      i ← indexes
+    } yield {
+      s ++ i
+    }
+  }
+
+}
---
+++ test/uk/gov/hmrc/fhregistration/repositories/SubmissionTrackingRepositorySpecs.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistration.repositories
+
+import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
+import org.scalatest.concurrent.Eventually
+import play.modules.reactivemongo.ReactiveMongoComponent
+import uk.gov.hmrc.mongo.{Awaiting, CurrentTime, MongoSpecSupport}
+import uk.gov.hmrc.play.test.LogCapturing
+
+class SubmissionTrackingRepositorySpecs
+  extends WordSpec
+    with Matchers
+    with MongoSpecSupport
+    with BeforeAndAfterEach
+    with Awaiting
+    with CurrentTime
+    with Eventually
+    with LogCapturing {
+
+  implicit val reactiveMongoComponent = new ReactiveMongoComponent {
+    override def mongoConnector = mongoConnectorForTest
+  }
+
+  val repository = new SubmissionTrackingRepository()
+
+
+  override protected def beforeEach(): Unit = {
+    await(repository.drop)
+    await(repository.ensureIndexes)
+  }
+
+  "Inserting a new record" should {
+    "Be successful" in {
+      val tracking = mkSubmissionTracking
+      await(repository.insertSubmissionTracking(tracking))
+
+      val byUserId = await(repository.findSubmissionTrackingByUserId(anUserId)).get
+      byUserId shouldEqual tracking
+
+      val byFormBunldeId = await(repository.findSubmissionTrakingByFormBundleId(aFormBundleId)).get
+      byFormBunldeId shouldEqual tracking
+
+    }
+
+  }
+
+  "Deleted a record" should {
+    "be successful" in {
+      val tracking = mkSubmissionTracking
+      await(repository.insertSubmissionTracking(tracking))
+
+      await(repository.deleteSubmissionTackingByFormBundleId(aFormBundleId))
+
+      val byUserId = await(repository.findSubmissionTrackingByUserId(anUserId))
+
+      byUserId shouldBe None
+
+
+    }
+  }
+
+  def mkSubmissionTracking = SubmissionTracking(
+    anUserId,
+    aFormBundleId,
+    anEmail,
+    System.currentTimeMillis()
+  )
+
+
+  val anUserId = "userid-1"
+  val unknownUserId = "userid-u"
+  val aFormBundleId: String = "012345678901"
+  val anEmail = "test@test.com"
+
+}
