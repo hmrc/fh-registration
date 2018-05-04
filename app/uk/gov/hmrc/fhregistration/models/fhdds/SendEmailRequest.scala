484f63e99472a5af90fc7bbf2a9539116bac0d44
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2018-05-04 11:06:14 +0100

    FHDDS-1035:added a test

--- app/uk/gov/hmrc/fhregistration/models/fhdds/SendEmailRequest.scala
+++ app/uk/gov/hmrc/fhregistration/models/fhdds/SendEmailRequest.scala
@@ -21,7 +21,5 @@ import play.api.libs.json._
 case class SendEmailRequest(to: List[String], templateId: String, parameters: Map[String, String], force: Boolean)
 
 object SendEmailRequest {
-  implicit val sendEmailRequestWrithes = new Writes[SendEmailRequest] {
-    override def writes(o: SendEmailRequest): JsValue = Json.writes[SendEmailRequest].writes(o)
-  }
+  implicit val format = Json.format[SendEmailRequest]
 }
