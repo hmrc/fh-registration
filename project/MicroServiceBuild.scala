f4b16a898b48dc0df9e40143be49a70c707303b4
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2019-11-18 14:37:22 +0000

    [CU][DLS-1033]: Upgraded libraries and removed ones that we don't use

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -10,10 +10,10 @@ object MicroServiceBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"             %% "bootstrap-play-26"            % "0.42.0",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"            % "1.1.0",
     "uk.gov.hmrc"             %% "simple-reactivemongo"         % "7.20.0-play-26",
     "com.github.tototoshi"    %% "play-json-naming"             % "1.3.0",
-    "com.eclipsesource"       %% "play-json-schema-validator"   % "0.8.9",
+    "com.eclipsesource"       %% "play-json-schema-validator"   % "0.9.4",
     "org.typelevel"           %% "cats"                         % "0.9.0"
   )
 
@@ -21,9 +21,8 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc"             %% "service-integration-test"     % "0.9.0-play-26" % scope,
     "org.scalatest"           %% "scalatest"                    % "3.0.8"   % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"           % "3.1.2"   % scope,
-    "org.mockito"              % "mockito-core"                 % "3.0.0"  % scope,
+    "org.mockito"              % "mockito-core"                 % "3.1.0"  % scope,
     "org.pegdown"              % "pegdown"                      % "1.6.0"   % scope,
-    "com.typesafe.play"       %% "play-test"                    % PlayVersion.current % scope,
     "uk.gov.hmrc"             %% "reactivemongo-test"           % "4.15.0-play-26"   % scope,
     "com.github.tomakehurst"   % "wiremock-jre8"                % "2.23.2"  % scope
 
