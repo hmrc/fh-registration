ae44a3399d78e04e6b2bf6c9e822d747f9401cea
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2024-03-11 15:46:03 +0000

    adding payload debug logs

--- app/uk/gov/hmrc/fhregistration/connectors/DesConnector.scala
+++ app/uk/gov/hmrc/fhregistration/connectors/DesConnector.scala
@@ -100,6 +100,7 @@ class DefaultDesConnector @Inject()(
     logger.info(s"Sending fhdds registration data to DES for safeId $safeId")
     implicit val headerCarrier: HeaderCarrier = headerCarrierBuilder(hc)
     http.POST[JsValue, HttpResponse](desSubmissionUrl(safeId), submission, headers = desHeaders).map { resp =>
+      logger.debug(s"Submission payload sent to DES - $submission")
       logger.info(s"DES response with status ${resp.status} and body - ${resp.body}")
       resp.json.as[DesSubmissionResponse]
     }
