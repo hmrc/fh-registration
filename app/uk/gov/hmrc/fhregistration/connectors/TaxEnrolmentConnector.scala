a14943a39ecc4ea956d5eaa4b72c5201bbb7df10
GinnyThomas <90277886+GinnyThomas@users.noreply.github.com>
2024-03-13 11:03:39 +0000

    DLS-10209 add back in Runtime Exception

--- app/uk/gov/hmrc/fhregistration/connectors/TaxEnrolmentConnector.scala
+++ app/uk/gov/hmrc/fhregistration/connectors/TaxEnrolmentConnector.scala
@@ -64,10 +64,11 @@ class DefaultTaxEnrolmentConnector @Inject()(
       .map { response =>
         if (is2xx(response.status)) {
           logger.info(s"Request to tax enrolments authorisation response: ${response.body}")
+          response
         }
-        else logger.warn(s"in tax enrolment subscribe, Unexpected response code '${response.status} with response body ${response.body}'")
-
-        response
+        else
+          logger.warn(s"in tax enrolment subscribe, Unexpected response code '${response.status} with response body ${response.body}'")
+          throw new RuntimeException(s"Unexpected response code '${response.status} with response body ${response.body}'")
       }
   }
 
