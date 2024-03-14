19e787324f6ab0ed885a75764cd5af98a63aa6b6
GinnyThomas <90277886+GinnyThomas@users.noreply.github.com>
2024-03-14 10:46:40 +0000

    DLS-10209 remove response body from send call

--- app/uk/gov/hmrc/fhregistration/connectors/DesConnector.scala
+++ app/uk/gov/hmrc/fhregistration/connectors/DesConnector.scala
@@ -99,8 +99,7 @@ class DefaultDesConnector @Inject()(
     logger.info(s"Sending fhdds registration data to DES for safeId $safeId")
     implicit val headerCarrier: HeaderCarrier = headerCarrierBuilder(hc)
     http.POST[JsValue, HttpResponse](desSubmissionUrl(safeId), submission, headers = desHeaders).map { resp =>
-      logger.debug(s"Submission payload sent to DES - $submission")
-      logger.info(s"DES response with status ${resp.status} and body - ${resp.body}")
+      logger.info(s"DES response with status ${resp.status}")
       resp.json.as[DesSubmissionResponse]
     }
   }
