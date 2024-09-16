0f9799db6daf1e4056ddded216524f1828f29097
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-09-16 15:36:21 +0100

    DLS-10268: Formats dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,17 +9,17 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
-    "org.typelevel" %% "cats-core" % "2.9.0",
-    "com.github.tototoshi" %% "play-json-naming" % "1.5.0",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion" % hmrcMongoVersion,
+    "uk.gov.hmrc"          %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
+    "org.typelevel"        %% "cats-core"                       % "2.9.0",
+    "com.github.tototoshi" %% "play-json-naming"                % "1.5.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.14" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.14" % Provided cross CrossVersion.full
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
   ).map(_ % "test, it")
 
   val all: Seq[ModuleID] = compile ++ test
