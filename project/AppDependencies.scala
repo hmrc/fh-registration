c277d9b381dbd091f1a94dbd0d69fc7c0bf7e428
ShivaniJain-21 <180167518+ShivaniJain-21@users.noreply.github.com>
2024-09-24 15:22:54 +0100

    DLS-10268 [FHDDS] updated dependencies and plugin versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt._
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "8.4.0"
-  val hmrcMongoVersion = "1.7.0"
+  val bootstrapVersion = "8.6.0"
+  val hmrcMongoVersion = "2.2.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"          %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
-    "org.typelevel"        %% "cats-core"                       % "2.9.0",
+    "org.typelevel"        %% "cats-core"                       % "2.12.0",
     "com.github.tototoshi" %% "play-json-naming"                % "1.5.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.14" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.14" % Provided cross CrossVersion.full
