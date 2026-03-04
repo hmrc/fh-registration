807f0f7d7bb7a820343d12b9d1a95fd8de66d7b8
jawadsaleem5747 <175114233+jawadsaleem5747@users.noreply.github.com>
2026-03-04 14:19:35 +0000

    DLS-12754: [FHDDS] Upgrade to bootstrap-play (10.6.0)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,20 @@ import sbt._
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "10.4.0"
-  val hmrcMongoVersion = "2.10.0"
+  val bootstrapVersion = "10.7.0"
+  val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"          %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
-    "org.typelevel"        %% "cats-core"                       % "2.12.0",
+    "org.typelevel"        %% "cats-core"                       % "2.13.0",
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
-    "org.scalatestplus.play" %% "scalatestplus-play"            % "7.0.1",
+    "org.scalatestplus.play" %% "scalatestplus-play"            % "7.0.2",
   ).map(_ % "test, it")
 
   val all: Seq[ModuleID] = compile ++ test
